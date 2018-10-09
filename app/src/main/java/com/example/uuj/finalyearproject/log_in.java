b152af4c5b77b1f715e7bf9b20c5b54508a86a49
Timothy McClelland <mcclelland-t1@email.ulster.ac.uk>
2018-10-09 18:13:22 +0100

    first commit

--- app/src/main/java/com/example/uuj/finalyearproject/log_in.java
+++ app/src/main/java/com/example/uuj/finalyearproject/log_in.java
@@ -3,11 +3,11 @@ package com.example.uuj.finalyearproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Register extends AppCompatActivity {
+public class log_in extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.register);
+        setContentView(R.layout.log_in);
     }
 }
