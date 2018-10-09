f1764a1d510c01faac498f3805f63bd90c928e4a
Timothy McClelland <mcclelland-t1@email.ulster.ac.uk>
2018-10-09 17:19:17 +0100

    third commit

--- app/src/main/java/com/example/uuj/finalyearproject/Register.java
+++ app/src/main/java/com/example/uuj/finalyearproject/Register.java
@@ -3,11 +3,11 @@ package com.example.uuj.finalyearproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.register);
     }
 }
