9ccc3543676e07fc5dbf6c29eeea0d51fe14a475
Timothy McClelland <mcclelland-t1@email.ulster.ac.uk>
2018-10-10 20:39:52 +0100

    sixth commit

--- app/src/main/java/com/example/uuj/finalyearproject/Register.java
+++ app/src/main/java/com/example/uuj/finalyearproject/Register.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -14,37 +15,19 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class Register extends AppCompatActivity {
+public class Register extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText email;
-    private EditText password;
-
-    private FirebaseAuth rAuth;
+    private TextView loginButton;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
-        email = (EditText) findViewById(R.id.editTextEmail);
-        password = (EditText) findViewById(R.id.editTextPassword);
-        rAuth = FirebaseAuth.getInstance();
+        loginButton = findViewById(R.id.textViewSignin);
+        loginButton.setOnClickListener(this);
     }
 
-    public void button_register(View v){
-        (rAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString()))
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    Toast.makeText(Register.this, "Registration Complete", Toast.LENGTH_SHORT).show();
-                    Intent i = new Intent(Register.this, log_in.class);
-                    startActivity(i);
-                }
-                else
-                {
-                    Log.e("ERROR", task.getException().toString());
-                    Toast.makeText(Register.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+    public void onClick(View arg0) {
+        Intent myIntent = new Intent(this, log_in.class);
+        startActivity(myIntent);
     }
 }
