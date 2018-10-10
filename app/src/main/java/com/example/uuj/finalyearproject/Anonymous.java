6f2b5bc426da9d76788d4a7afa3447930718fd2d
Timothy McClelland <mcclelland-t1@email.ulster.ac.uk>
2018-10-10 12:58:52 +0100

    fourth commit

--- app/src/main/java/com/example/uuj/finalyearproject/Anonymous.java
+++ app/src/main/java/com/example/uuj/finalyearproject/Anonymous.java
@@ -3,7 +3,7 @@ package com.example.uuj.finalyearproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Anonymous extends AppCompatActivity {
+public class Anonymous extends log_in {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
