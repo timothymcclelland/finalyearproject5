e690a73e143ebc3bb621ad6ce69e875c3f0a7768
Timothy McClelland <mcclelland-t1@email.ulster.ac.uk>
2018-10-22 18:34:56 +0100

    code for forgot_Password classes

--- app/src/main/java/com/example/uuj/finalyearproject/reset_password.java
+++ app/src/main/java/com/example/uuj/finalyearproject/reset_password.java
@@ -16,48 +16,30 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class reset_password extends AppCompatActivity {
 
-    private static final String TAG = "EmailPasswordAuth";
+    private EditText resetEmail;
+    private Button resetPassword;
 
-    Button resetPassword;
-
-    FirebaseAuth firebaseAuth;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        resetPassword.findViewById(R.id.buttonReset);
         resetPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (v == resetPassword) {
-                    sendResetPasswordEmail();
+                if(v == resetPassword){
+                    resetPassword();
                 }
             }
         });
     }
 
-    private void sendResetPasswordEmail() {
-        final String email = ((EditText) findViewById(R.id.editTextEmailReset))
-                .getText().toString();
-        firebaseAuth.sendPasswordResetEmail(email)
-                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
+    public void resetPassword() {
 
-                        if (task.isSuccessful()) {
-                            Toast.makeText(reset_password.this,
-                                    "Reset password code has been emailed to "
-                                            + email,
-                                    Toast.LENGTH_SHORT).show();
-                        } else {
-                            Log.e(TAG, "Error in sending reset password code",
-                                    task.getException());
-                            Toast.makeText(reset_password.this,
-                                    "There is a problem with reset password, try later.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
     }
 }
