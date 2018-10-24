ad93582c17724bbd902cbe4c64be4d0c15cfe43a
Timothy McClelland <mcclelland-t1@email.ulster.ac.uk>
2018-10-24 16:22:43 +0100

    toolbar and bottom navigation added

--- app/src/main/java/com/example/uuj/finalyearproject/content.java
+++ app/src/main/java/com/example/uuj/finalyearproject/content.java
@@ -1,13 +1,24 @@
 package com.example.uuj.finalyearproject;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class content extends AppCompatActivity {
+    private Toolbar toolbar;
+    private BottomNavigationView bottomNav;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content);
+
+        toolbar.findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        bottomNav.findViewById(R.id.bottom_navigation);
     }
 }
