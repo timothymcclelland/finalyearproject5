fe05336dbc954106898c735326eb2ab92036dbf6
Timothy McClelland <mcclelland-t1@email.ulster.ac.uk>
2018-10-29 14:10:14 +0000

    floating action button & toolbar layout update

--- app/src/main/java/com/example/uuj/finalyearproject/content.java
+++ app/src/main/java/com/example/uuj/finalyearproject/content.java
@@ -1,17 +1,80 @@
 package com.example.uuj.finalyearproject;
 
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class content extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        Toolbar mytoolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mytoolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        FloatingActionButton postButton = (FloatingActionButton)findViewById(R.id.float_post);
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                postContent();
+            }
+        });
+    }
+
+    public void postContent() {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater mMenuInflater = getMenuInflater();
+        mMenuInflater.inflate(R.menu.toolbar_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_filter:
+                filter();
+                return true;
+            case R.id.action_sort:
+                sort();
+                return true;
+            case R.id.action_sign_out:
+                signOut();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void signOut(){
+        mAuth.signOut();
+        finish();
+        startActivity(new Intent(this, log_in.class));
+    }
+
+    public void sort(){
+
+    }
+
+    public void filter(){
+
     }
 }
